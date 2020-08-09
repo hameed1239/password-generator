407eeb1ea0a4416e45c7bef6bc41b965c4636c9b
hameed kazeem <hameed.kazeem@gmail.com>
2020-08-08 23:30:10 -0400

    add function to return password

--- script.js
+++ script.js
@@ -14,161 +14,398 @@ var randomPassword = [];
 
 
 //check typeof data
-function checkTypeof(data){
+function checkTypeof(data) {
   return typeof data;
 }
 
- // Get desired password length: returns password Length
-function getPasswordLength(){
+// Get desired password length: returns password Length
+function getPasswordLength() {
 
   do {
-    passwordLength = parseInt(window.prompt ("Enter desired password length between 8 and 128"));
-   }while (!isValidNum(passwordLength));
+    passwordLength = parseInt(window.prompt("Enter desired password length between 8 and 128"));
+  } while (!isValidNum(passwordLength));
 
 }// end get password length
 
 
-function isValidNum (num){
-   //check is input is a number
-   if (typeof num === 'number' && num >=8 && num <= 128 ){
+function isValidNum(num) {
+  //check is input is a number
+  if (typeof num === 'number' && num >= 8 && num <= 128) {
     //  console.log("true");
     return true;
-   }
-   else{
+  }
+  else {
     // console.log("false");
-     return false;
-   }
+    return false;
+  }
 }// end input checker
 
-function includeUppercase(){
-  return window.confirm ("Should the password contain uppercase characters?");
+function includeUppercase() {
+  return window.confirm("Should the password contain uppercase characters?");
 }
 
 // include lowercase characters?
-function includeLowercase(){
- return window.confirm ("Should the password contain lowercase characters?");
+function includeLowercase() {
+  return window.confirm("Should the password contain lowercase characters?");
 }
 
 // include numbers characters?
-function includeNumbers(){
-  return window.confirm ("Should the password contain numbers?");
+function includeNumbers() {
+  return window.confirm("Should the password contain numbers?");
 }
 
 // include lowercase characters?
-function includeSpecialChar(){
-  return window.confirm ("Should the password contain special characters?");
+function includeSpecialChar() {
+  return window.confirm("Should the password contain special characters?");
 }
 
 //verify that atleast one criteria is true
-function isValidCriteria(isUpper, isLower, isNum, isSpecial){
-  if (isNum||isUpper||isLower||isSpecial){
+function isValidCriteria(isUpper, isLower, isNum, isSpecial) {
+  if (isNum || isUpper || isLower || isSpecial) {
     return true;
   }
-  else{
+  else {
     return false;
   }
 }
 //GET RANDOM NUMBER
-function randomNumber(){
+function randomNumber() {
   return Math.floor(Math.random() * 10);
 }
 
 //Get random lowercase
-function randomLowercase(){
-  return lowercase.charAt(Math.floor(Math.random() * lowercase.length)+1)
+function randomLowercase() {
+  return lowercase.charAt(Math.floor(Math.random() * lowercase.length) + 1)
 }
 
 //Get random uppercase
-function randomUppercase(){
-  return uppercase.charAt(Math.floor(Math.random() * uppercase.length)+1)
+function randomUppercase() {
+  return uppercase.charAt(Math.floor(Math.random() * uppercase.length) + 1)
 }
 
 //Get random special character
-function randomSpecialCharacter(){
-  return specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length) +1)
+function randomSpecialCharacter() {
+  return specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length) + 1)
 }
 
 
 //get password criteria combination to randomize 
-function getPasswordCombination(){
+function getPasswordCombination() {
   var combo = 0;
-  if (isUppercase){
-    combo+=2;
+  if (isUppercase) {
+    combo += 2;
   }
-  if (isLowercase){
-    combo+=1;
+  if (isLowercase) {
+    combo += 1;
   }
-  if (isNumbers){
-    combo+=4;
+  if (isNumbers) {
+    combo += 4;
   }
-  if (isSpecialCharacter){
-    combo+=8;
+  if (isSpecialCharacter) {
+    combo += 8;
   }
   return combo;
 }
 
-function passwordGenerator(){
-  switch (getPasswordCombination()){
+function passwordGenerator() {
+  switch (getPasswordCombination()) {
     case 0: //it would never be called. 
       break;
 
     case 1: // Lowercase only
-      for ( var i = 0; i < passwordLength; i++){
+      for (var i = 0; i < passwordLength; i++) {
         randomPassword[i] = randomLowercase();
       }
       break;
 
     case 2: // uppercase only
-    for ( var i = 0; i < passwordLength; i++){
-      randomPassword[i] = randomUppercase();
-    }
+      for (var i = 0; i < passwordLength; i++) {
+        randomPassword[i] = randomUppercase();
+      }
       break;
 
     case 3: // Lowercase and uppercase 
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 3) {
+          randomPassword[i] = randomLowercase();
+        }
+        else if (i === 7) {
+          randomPassword[i] = randomUppercase();
+        }
+
+        else { // Randomized assignment
+          if (Math.floor(Math.random() * 2) === 1) {
+            randomPassword[i] = randomLowercase();
+          }
+          else {
+            randomPassword[i] = randomUppercase();
+          }
+        }
+      }
       break;
 
     case 4: // Numbers only
-    for ( var i = 0; i < passwordLength; i++){
-      randomPassword[i] = randomNumber();
-    }
+      for (var i = 0; i < passwordLength; i++) {
+        randomPassword[i] = randomNumber();
+      }
       break;
 
     case 5: // Numbers and Lowercase 
-      break;
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 2) {
+          randomPassword[i] = randomLowercase();
+        }
+        else if (i === 6) {
+          randomPassword[i] = randomUppercase();
+        }
 
-    case 6: // Numbers and uppercase 
-      break;
-
-    case 7: // Numbers, uppercase and lowercase
-      break;
-
-    case 8: // Special characters only
-    for ( var i = 0; i < passwordLength; i++){
-      randomPassword[i] = randomSpecialCharacter();
+        else { // Randomized assignment
+        if (Math.floor(Math.random() * 2) === 1) {
+          randomPassword[i] = randomNumber();
+        }
+        else {
+          randomPassword[i] = randomLowercase();
+        }
+      }
     }
       break;
 
+    case 6: // Numbers and uppercase 
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 1) {
+          randomPassword[i] = randomNumber();
+        }
+        else if (i === 4) {
+          randomPassword[i] = randomUppercase();
+        }
+
+        else { // Randomized assignment
+        if (Math.floor(Math.random() * 2) === 1) {
+          randomPassword[i] = randomNumber();
+        }
+        else {
+          randomPassword[i] = randomUppercase();
+        }
+      }
+    }
+      break;
+
+    case 7: // Numbers, uppercase and lowercase
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 2) {
+          randomPassword[i] = randomLowercase();
+        }
+        else if (i === 7) {
+          randomPassword[i] = randomUppercase();
+        }
+        else if (i === 3){
+          randomPassword[i] = randomNumber();
+        }
+
+        else { // Randomized assignment
+        var x = Math.floor(Math.random() * 3);
+        if (x === 0) {
+          randomPassword[i] = randomNumber();
+        }
+        else if (x = 1) {
+          randomPassword[i] = randomUppercase();
+        }
+        else {
+          randomPassword[i] = randomLowercase();
+        }
+      }
+    }
+      break;
+
+    case 8: // Special characters only
+      for (var i = 0; i < passwordLength; i++) {
+        randomPassword[i] = randomSpecialCharacter();
+      }
+      break;
+
     case 9: // Special characters and lowercase
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 2) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else if (i === 6) {
+          randomPassword[i] = randomLowercase();
+        }
+
+        else { // Randomized assignment
+        if (Math.floor(Math.random() * 2) === 1) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else {
+          randomPassword[i] = randomLowercase();
+        }
+      }
+    }
       break;
 
     case 10: // special characters and uppercase 
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 1) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else if (i === 4) {
+          randomPassword[i] = randomUppercase();
+        }
+
+        else { // Randomized assignment
+        if (Math.floor(Math.random() * 2) === 1) {
+          randomPassword[i] = randomUppercase();
+        }
+        else {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+      }
+    }
       break;
 
     case 11: // special characters, uppercase, and lowercase
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 2) {
+          randomPassword[i] = randomLowercase();
+        }
+        else if (i === 5) {
+          randomPassword[i] = randomUppercase();
+        }
+        else if (i === 8){
+          randomPassword[i] = randomSpecialCharacter();
+        }
+
+        else { // Randomized assignment
+        var x = Math.floor(Math.random() * 3);
+        if (x === 0) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else if (x === 1) {
+          randomPassword[i] = randomUppercase();
+        }
+        else {
+          randomPassword[i] = randomLowercase();
+        }
+      }
+    }
       break;
 
     case 12: // special characters, and numbers
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 3) {
+          randomPassword[i] = randomNumber();
+        }
+        else if (i === 5) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+
+        else { // Randomized assignment
+        if (Math.floor(Math.random() * 2) === 1) {
+          randomPassword[i] = randomNumber();
+        }
+        else {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+      }
+    }
       break;
 
     case 13: // Lowercase, numbers, and special characters 
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 2) {
+          randomPassword[i] = randomLowercase();
+        }
+        else if (i === 7) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else if (i === 3){
+          randomPassword[i] = randomNumber();
+        }
+
+        else { // Randomized assignment
+        var x = Math.floor(Math.random() * 3);
+        if (x === 0) {
+          randomPassword[i] = randomNumber();
+        }
+        else if (x === 1) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else {
+          randomPassword[i] = randomLowercase();
+        }
+      }
+    }
       break;
 
     case 14: // Uppercase, numbers, and special characters
+      for (var i = 0; i < passwordLength; i++) {
+        // Force assignments
+        if (i === 2) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else if (i === 3) {
+          randomPassword[i] = randomUppercase();
+        }
+        else if (i === 8){
+          randomPassword[i] = randomNumber();
+        }
+
+        else { // Randomized assignment
+        var x = Math.floor(Math.random() * 3);
+        if (x === 0) {
+          randomPassword[i] = randomNumber();
+        }
+        else if (x === 1) {
+          randomPassword[i] = randomUppercase();
+        }
+        else {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+      }
+    }
       break;
 
     case 15: // Lowercase, uppercase, numbers, and special characters 
+      for (var i = 0; i < passwordLength; i++) {
+        var x = Math.floor(Math.random() * 4);
+        if (i === 1) {
+          randomPassword[i] = randomNumber();
+        }
+        else if (i === 3) {
+          randomPassword[i] = randomLowercase();
+        }
+        else if (i === 5) {
+          randomPassword[i] = randomSpecialCharacter();
+        }
+        else if (i === 7) {
+          randomPassword[i] = randomUppercase();
+        }
+        else {
+          if (x === 0) {
+            randomPassword[i] = randomNumber();
+          }
+          else if (x === 1) {
+            randomPassword[i] = randomUppercase();
+          }
+          else if (x === 2) {
+            randomPassword[i] = randomLowercase();
+          }
+          else {
+            randomPassword[i] = randomSpecialCharacter();
+          }
+        }
+      }
       break;
-    
+
   }
 }
 
@@ -180,18 +417,19 @@ function writePassword() {
   isLowercase = includeLowercase();
   isNumbers = includeNumbers();
   isSpecialCharacter = includeSpecialChar();
-    
+
   // isValidCrite = isValidCriteria();
-  if (isValidCriteria(isUppercase, isLowercase, isNumbers, isSpecialCharacter)){
+  if (isValidCriteria(isUppercase, isLowercase, isNumbers, isSpecialCharacter)) {
     passwordCombination = getPasswordCombination();
     passwordGenerator();
     var passwordText = document.querySelector("#password");
 
+    passwordText.value = "";
     passwordText.value = `${passwordLength} ${isUppercase} ${isLowercase} ${isNumbers} ${isSpecialCharacter} ${passwordCombination} ${randomPassword}`;
     //passwordText.value = password;
 
   }
-  else{
+  else {
     window.alert("Paswword must contain atleast uppercase, lowercase, number or special characters");
   }
 
